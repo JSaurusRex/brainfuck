5ba076ee85f30a1f0b89f2840a2f596bc3af6898
JSaurusRex <jessetonckens74298215@gmail.com>
2019-12-06 15:37:10 +0100

    fixed bugs

--- main.c
+++ main.c
@@ -8,23 +8,27 @@
 char * code;
 int code_length;
 
-int * intarray; //all variables (array)
+char * intarray; //all variables (array)
 int currentarray; //what variable is currently in use
 int * colon; //what the position is of the last colon (array)
-int lastcolon;
+int lastcolon = 0;
 int position;
 
 int show_commands = 0;
-
+int show_values = 0;
 void run_code();
-void load_file (char *File);
+void load_file (const char *File);
 
 
 int main(int argc, char **argv)
 {
     code = malloc(sizeof(char)*MAXCHAR);
-    intarray = malloc(sizeof(int)*MAXINT);
-    colon = malloc(sizeof(int)*MAXCOLON);
+    intarray = (char*) calloc(MAXINT, sizeof(char));
+    colon = (int*) calloc(MAXCOLON, sizeof(int));
+
+
+
+
 
     //checks arguments
     int isempty = 0;
@@ -32,18 +36,28 @@ int main(int argc, char **argv)
     for (int i = 1  ; i < argc; ++i)
     {
         //printf("%s ", argv[i]);
-        char * temp = argv[i];
-        if(strcmp(argv[i], "-l") == 0) {load_file(argv[i+1]); isempty=1;}
-        else if(strcmp(argv[i], "-s") == 0) {show_commands = 1; printf("\nbecause of -s, now showing process\n");}
+
+        if(argv[i][0] == '-'){
+            int max = strlen(argv[i]);
+            for(int j = 1; j < max; j++)
+            {
+                if(argv[i][j] == 's') {show_commands = 1; printf("\nBecayse of -s, now showing process");}
+                if(argv[i][j] == 'v') {show_values = 1; printf("\nBecayse of -v, now showing values");}
+
+            }
+        }
+
 	else {load_file(argv[i]); isempty = 1;}
         //if(argv[i] == "-l") {loadfile("./text.txt");}
     }
     if(!isempty) {
-	//printf("error: no file specified\n");
-	return 0;
+        printf("error: no file specified\n");
+        load_file("helloworld.txt");
+        //return 0;
     }
 
     //printf("Starting emulation..\n");
+    position = 0;
 
     while(code[position] != '\0' && position < MAXCHAR)
     {
@@ -62,35 +76,36 @@ void run_code()
     if(code[position] == '<') {
         currentarray--;
         if(currentarray < 0) currentarray = MAXCOLON;
-        if(currentarray > MAXCOLON) currentarray = 0;
+        if(show_values == 1) printf("|%i|", currentarray);
         return;}
     if(code[position] == '>') {
-        currentarray--;
-        if(currentarray < 0) currentarray = MAXCHAR;
+        currentarray++;
         if(currentarray > MAXCHAR) currentarray = 0;
+        if(show_values == 1) printf("|%i|", currentarray);
         return;}
 
-    if(code[position] == '+') {intarray[currentarray]++; if(intarray[currentarray] > 255) intarray[currentarray]=0; return;}
-    if(code[position] == '-') {intarray[currentarray]--; if(intarray[currentarray] < 0) intarray[currentarray]=255; return;}
+    if(code[position] == '+') {intarray[currentarray]++;return;}
+    if(code[position] == '-') {intarray[currentarray]--; if(intarray[currentarray] < (char) 0) intarray[currentarray]=(char) 255; return;}
 
-    if(code[position] == '.') {printf("%c%i", intarray[currentarray], intarray[currentarray]); return;}
-    if(code[position] == ',') {scanf("%c", &intarray[currentarray]); return;}
+
+    if(code[position] == '.') {printf("%c", intarray[currentarray]); if(show_values == 1) printf("(%i)", intarray[currentarray]); return;}
+    if(code[position] == ',') {scanf("%c",&intarray[currentarray]); return;}
 
     if(code[position] == '[') {lastcolon++; colon[lastcolon] == position; return;}
     if(code[position] == ']') {
         if(intarray[currentarray] <= 0){
             //if the loop gets exited
             lastcolon--;
-            if(lastcolon < 0) printf("there is an error in your program, ] is before [");
+            if(lastcolon < 0) printf("\nthere is an error in your program, ] is before [\n");
             return;
         }else{
             //if it loops
             position = colon[lastcolon];
-            printf("%d", position);
+            //printf("%d", position);
     }}
 }
 
-void load_file (char *File)
+void load_file (const char *File)
 {
     printf("loading file\n");
     FILE *fp;
