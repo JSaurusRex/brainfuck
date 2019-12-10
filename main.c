cb4c969a606997392fe093bb4c190f317af9a06d
JSaurusRex <jessetonckens74298215@gmail.com>
2019-12-10 18:22:53 +0100

    added new kind of unfinished optimization

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #define MAXINT 1000
-#define MAXCOLON 10000
+#define MAXCOLON 1000
 #define MAXBUFFER 50
 char * code;
 int code_length;
@@ -11,6 +11,7 @@ int code_length;
 unsigned char * intarray; //all variables (array)
 int currentarray; //what variable is currently in use
 int * colon; //what the position is of the last colon (array)
+int * colonClose;
 int lastcolon = 0;
 int position;
 
@@ -23,10 +24,85 @@ unsigned char * buffer;
 int display = 0;
 
 
+void (*funcs[200]) ();
+
+void plus1 ()
+{
+    printf("plus1 ");
+    intarray[currentarray]++;
+}
+
+void plus2 ()
+{
+    printf("plus2 ");
+    intarray[currentarray]+=2;
+}
+
+void plus5()
+{
+    printf("plus5 ");
+    intarray[currentarray]+=5;
+}
+
+void min1 ()
+{
+    printf("min1 ");
+    intarray[currentarray]--;
+}
+
+void min2 ()
+{
+    printf("min2 ");
+    intarray[currentarray]-=2;
+}
+
+void min5()
+{
+    printf("min5 ");
+    intarray[currentarray]-=5;
+}
+
+void loopOpen()
+{
+    printf("loopOpen ");
+    lastcolon++;
+    if(intarray[currentarray] == 0)
+    {
+        position = colonClose[lastcolon];
+        lastcolon--;
+    }
+}
+
+void loopClose ()
+{
+    printf("loopClose ");
+    if(intarray[currentarray] != 0)
+    {
+        position = colon[lastcolon];
+    }else lastcolon--;
+}
+
+void point ()
+{
+    addtoBuffer(intarray[currentarray]);
+}
+
+void moveRight ()
+{
+    intarray++;
+}
+
+void moveLeft ()
+{
+    intarray--;
+}
+
+
 int main(int argc, char **argv)
 {
     intarray = (unsigned char*) calloc(MAXINT, sizeof(unsigned char));
     colon = (int*) calloc(MAXCOLON, sizeof(int));
+    colonClose = (int*) calloc(MAXCOLON, sizeof(int));
 
 
 
@@ -67,19 +143,23 @@ int main(int argc, char **argv)
     //printf("Starting emulation..\n");
     position = 0;
 
+
     while(code[position] != '\0' && position < code_length)
     {
-        run_code();
+        //run_code();
+        (*funcs[position]) ();
         position++;
     }
     displayBuffer();
     printf("\nend of program");
     free(intarray);
     free(colon);
+    free(colonClose);
     free(buffer);
     //printf("type anything to continue");
     return 0;
 }
+
 
 void run_code()
 {
@@ -175,8 +255,113 @@ void displayBuffer()
     display = 0;
 }
 
+int intcount = 0;
+int pos = 0;
+int colonCount= 0;
+void compile (char c)
+{
+    printf("\n%c pos:%i  intcount: %i", c, pos, intcount);
+    if(intcount != 0)
+    {
+        if(c != '-' && c != '+')
+        {
+            printf("\n");
+            if(intcount < 0)
+            {
+                while(intcount <= -5)
+                {
+                    printf(" -5");
+                    intcount+=5;
+                    funcs[pos]=&min5;
+                    pos++;
+                }
+                while(intcount <= -2)
+                {
+                    printf(" -2");
+                    intcount+=2;
+                    funcs[pos]=&min2;
+                    pos++;
+                }
+                while(intcount <= -1)
+                {
+                    printf(" -1");
+                    intcount++;
+                    funcs[pos]=&min1;
+                    pos++;
+                }
+            }
+        if(intcount > 0)
+            {
+                while(intcount >= 5)
+                {
+                    printf(" +5");
+                    intcount-=5;
+                    funcs[pos]=&plus5;
+                    pos++;
+                }
+                while(intcount >= 2)
+                {
+                    printf(" +2");
+                    intcount-=2;
+                    funcs[pos]=&plus2;
+                    pos++;
+                }
+                while(intcount >= 1)
+                {
+                    printf(" +1");
+                    intcount--;
+                    funcs[pos]=&plus1;
+                    pos++;
+                }
+            }
+        }
+    }
+    if(c == '+') intcount++;
+    if(c == '-') intcount--;
+
+    if(c == '>')
+    {
+        printf(" move right");
+        funcs[pos] = &moveLeft;
+        pos++;
+    }
+
+    if(c == '<')
+    {
+        printf(" move left");
+        funcs[pos] = &moveRight;
+        pos++;
+    }
+
+    if(c == '[')
+    {
+        printf(" loop open");
+        colonCount++;
+        funcs[pos] = &loopOpen;
+        colon[colonCount] = pos;
+        pos++;
+    }
+
+    if(c == ']')
+    {
+        printf(" loop close");
+        funcs[pos] = &loopOpen;
+        colonClose[colonCount] = pos;
+        colonCount--;
+        pos++;
+    }
+
+    if(c == '.')
+    {
+        funcs[pos] = &point;
+        pos++;
+    }
+
+}
+
 void load_file (char * file)
 {
+    pos = 0;
     FILE *fp;
     fp = fopen(file, "r"); // read mode
     rewind(fp);
@@ -195,11 +380,13 @@ void load_file (char * file)
     while(i < size)
     {
         ch = fgetc(fp);
-        file_output[i] = ch;
+        //file_output[i] = ch;
+        compile(ch);
         i++;
     }
     fclose(fp);
-    code = file_output;
+    printf("\ncompiled");
+    //code = file_output;
     code_length = size;
     return;
 }
